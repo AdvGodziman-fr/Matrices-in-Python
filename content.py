a8f78272370d39ee964c12c9a3b306ef1691509f
AdvGodziman-fr <advik9784@gmail.com>
2025-11-23 20:15:32 +0530

    Update content.py

--- content.py
+++ content.py
@@ -2,136 +2,126 @@ import numpy as np
 
 def create_matrix():
 
-    global rows
-    global cols
+    while True:
+        try:
+            rows = int(input('Enter the number of rows: '))
+            break
+        except ValueError:
+            print('The value of rows should be an integer and not any other data type')
 
-    print
-    try:
-        rows = int(input('Enter the number of rows: '))
-    except ValueError:
-        print('The value of rows should be a integer and not any other data type')
-        rows = int(input('Enter the number of rows: '))
-
-    try:
-        cols = int(input('Enter the number of columns: '))
-    except:
-        print('The value of rows should be a integer and not any other data type')
-        cols = int(input('Enter the number of columns: '))
-
-        
+    while True:
+        try:
+            cols = int(input('Enter the number of columns: '))
+            break
+        except ValueError:
+            print('The value of columns should be an integer and not any other data type')
 
     matrix = []
     print('Enter the elements for the matrix')
     print()
-        
     for i in range(rows):
         while True:
             row_eles = input(f"Enter {cols} elements for row {i + 1} separating them using commas: ")
             raw_rows = row_eles.split(',')
-
             if len(raw_rows) != cols:
-                print(f'Input of columns = {cols}, but no. of elements is greater pf le')
+                print(f'Input of columns = {cols}, but number of elements entered is {len(raw_rows)}')
                 continue
-
-            float_row = [float(j) for j in raw_rows]
-            np.array(matrix.append(float_row))
+            try:
+                float_row = [float(j) for j in raw_rows]
+            except ValueError:
+                print('Please enter only numbers.')
+                continue
+            matrix.append(float_row)
             break
-
-    return matrix
+    return np.array(matrix)
  
+
 class Matrix_Operations(object):
     def __init__(self):
         print('INSTRUCTIONS FOR Matrix MATRIX OPERATIONS')
         print('\n1. For Addition and Subtraction of Matrix, the rows and columns of the matrices should be same.')
-        print('2. For Multiplication (Matrix) of Mat1(n * m) and Mat2(p * q), n = q and m = p')
+        print('2. For Multiplication (Matrix) of Mat1(n * m) and Mat2(p * q), m = p')
         print()
         print('*****************************************************************************')
-        
-        global mat1
-        global mat2
 
         print('\nCreate the first Matrix:')
         print()
-        mat1 = create_matrix()
+        self.mat1 = create_matrix()
 
         print('\nCreate the second Matrix:')
         print()
-        mat2 = create_matrix()
+        self.mat2 = create_matrix()
 
     def addition(self):
         print('\nMatrix Addition:')
         print('------------------------------------')
         try:
-            add = np.add(mat1, mat2)
-            print(f'The addition of Mat1 {mat1} and Mat 2 {mat2} = {add}')
-        except:
+            add = np.add(self.mat1, self.mat2)
+            print(f'The addition of Mat1 {self.mat1} and Mat 2 {self.mat2} = {add}')
+        except Exception:
             print('Error: There is a mismatch in the number of rows and columns in the matrices')
             mat_add1 = create_matrix()
             mat_add2 = create_matrix()
-            np.add(mat_add1, mat_add2)
+            add = np.add(mat_add1, mat_add2)
             print(f'The addition of Mat1 {mat_add1} and Mat 2 {mat_add2} = {add}')
-            print('******************************************')
+        print('******************************************')
     
     def subtraction(self):
         print('\nMatrix Subtraction:')
         print('------------------------------------')
         try:
-            sub = np.subtract(mat1, mat2)
-            print(f'The subtraction of Mat1 {mat1} and Mat 2 {mat2} = {sub}')
-        except:
+            sub = np.subtract(self.mat1, self.mat2)
+            print(f'The subtraction of Mat1 {self.mat1} and Mat 2 {self.mat2} = {sub}')
+        except Exception:
             print('Error: There is a mismatch in the number of rows and columns in the matrices')
             mat_sub1 = create_matrix()
             mat_sub2 = create_matrix()
             sub = np.subtract(mat_sub1, mat_sub2)
             print(f'The subtraction of Mat1 {mat_sub1} and Mat 2 {mat_sub2} = {sub}')
-            print('\n******************************************')
+        print('\n******************************************')
 
     def scalar_matrix_multiplications(self):
         print('\nScalar-Matrix Multiplications:')
         print('------------------------------------')
-        try:
-            scalar = int(input("Enter a scalar value: "))
-        except:
-            print('Enter a proper Integer Value to Multipy with the Matrix')
-            scalar = int(input("Enter a scalar value: "))
-        scalar_mul1 = scalar * mat1
-        scalar_mul2 = scalar * mat2
-        
-        print(f'The Muliplication of {scalar} * {mat1} = {scalar_mul1}')
-        print(f'The Muliplication of {scalar} * {mat2} = {scalar_mul2}')
+        while True:
+            try:
+                scalar = float(input("Enter a scalar value: "))
+                break
+            except ValueError:
+                print('Enter a proper numeric value to multiply with the Matrix')
+        scalar_mul1 = scalar * self.mat1
+        scalar_mul2 = scalar * self.mat2
+        print(f'The Multiplication of {scalar} * {self.mat1} = {scalar_mul1}')
+        print(f'The Multiplication of {scalar} * {self.mat2} = {scalar_mul2}')
         print('\n******************************************')
     
     def matrix_multiplication(self):
         print('\nMatrix Multiplication:')
         print('------------------------------------')
-        shape1 = list(np.shape(mat1))
-        shape2 = list(np.shape(mat2))
-        print(shape1, shape2)
+        shape1 = list(np.shape(self.mat1))
+        shape2 = list(np.shape(self.mat2))
         try:
-            print(np.matmul(mat1, mat2))
-        except:
+            result = np.matmul(self.mat1, self.mat2)
+            print(f'The Multiplication of {self.mat1} * {self.mat2} = {result}')
+        except Exception:
             print('Format Not Followed')
-            print('Enter a Proper Format i.e. for matricies m*n and p*q, m = q and n = p')
-            print('Shape of Matrix 1 = ', shape1)
-            print('Shape of Matrix 2 = ', shape2)
+            print('Enter a Proper Format i.e. for matrices m*n and p*q, m = p')
             matmul1 = create_matrix()
             matmul2 = create_matrix()
-            print(f'The Multiplication of {mat1} * {mat2} = {np.matmul(matmul1, matmul2)}')
-            print('\n******************************************')
+            print(f'The Multiplication of {matmul1} * {matmul2} = {np.matmul(matmul1, matmul2)}')
+        print('\n******************************************')
 
     def inverse_matrix(self):
         print('\nMatrix Inversion:')
         try:
-            mat1_ = np.linalg.inv(mat1)
-            mat2_ = np.linalg.inv(mat2)
-        except:
+            mat1_ = np.linalg.inv(self.mat1)
+            mat2_ = np.linalg.inv(self.mat2)
+        except Exception:
             print('Matrix is singular and cannot be inverted. Please enter new matrices.')
             mat1 = create_matrix()
             mat2 = create_matrix()
             mat1_ = np.linalg.inv(mat1)
             mat2_ = np.linalg.inv(mat2)
-
-
         print(f'Inverse of Matrix 1 = {mat1_}')
         print(f'Inverse of Matrix 2 = {mat2_}')
         print('\n******************************************')
@@ -139,30 +129,29 @@ class Matrix_Operations(object):
     def transpose_elements(self):
         print('\nMatrix Transposition:')
         print('--------------------------------------')
-        trans_mat1_ = np.transpose(mat1)
-        trans_mat2_ = np.transpose(mat2)
+        trans_mat1_ = np.transpose(self.mat1)
+        trans_mat2_ = np.transpose(self.mat2)
         print(f'Transposed Matrix 1 = {trans_mat1_}')
         print(f'Transposed Matrix 2 = {trans_mat2_}')
-
         print('\n******************************************')
+
 
     def determinants(self):
         print('\n Finding Determinants')
         print('--------------------------------------')
         try:
-            det1 = np.linalg.det(mat1)
-            det2 = np.linalg.det(mat2)
-        except:
+            det1 = np.linalg.det(self.mat1)
+            det2 = np.linalg.det(self.mat2)
+        except Exception:
             print('The Matrix should be a square Matrix')
             mat1 = create_matrix()
             mat2 = create_matrix()
             det1 = np.linalg.det(mat1)
             det2 = np.linalg.det(mat2)
-
-
         print(f'Determinants of Matrix 1 = {det1}')
         print(f'Determinants of Matrix 2 = {det2}')
         print('\n******************************************')
+
 
 workspace = Matrix_Operations()
 workspace.addition()
